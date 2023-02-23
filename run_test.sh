e9384fa160b9042f7f9fb1cb8dcd77cfb3d3b9ac
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2023-02-22 21:35:33 -0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -17,8 +17,15 @@ docker-compose -f docker-compose.yml up --build --detach --remove-orphans
 printf "\n\nCheck Pip8\n"
 pycodestyle
 
-printf "\n\nRunning Tests\n"
+printf "\n\nExport Services EndPoints\n"
+export ELASTICSEARCH_URL='http://localhost:9200'
+export IMATCH_URL='http://localhost:8099'
+
+printf "\n\nCheck Services\n"
+python wait_for_elastic.py
 python wait_for_imatch.py
+
+printf "\n\nRunning Tests\n"
 pytest $1 $2
 
 # printf "\n\nStoping containers"
