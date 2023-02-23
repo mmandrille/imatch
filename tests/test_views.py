0ee093d6a3bd9fc4f544b6f070e93bb6a4a831ac
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2023-02-22 23:41:42 -0300

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,16 +1,24 @@
 # Standard Imports
+import os
 import json
+import time
+import logging
 import requests
 # Extra imports
 import pytest
 from elasticsearch import Elasticsearch
 
 
+# Logger
+LOG_LEVEL = int(os.getenv('DEBUG_LEVEL', logging.INFO))
+logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s|%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
 # Globals
 IMATCH_URL = "http://localhost:8099"
 ELASTICSEARCH_URL = "http://localhost:9200"
 ELASTICSEARCH_INDEX = "imatch"
-
+ELASTIC_DELAY = 1
 TIMEOUT = 10
 URL1 = 'https://ichef.bbci.co.uk/news/800/cpsprodpb/15665/production/_107435678_perro1.jpg'
 URL2 = 'https://www.clinicaveterinariaaguilar.es/wp-content/uploads/2020/01/cachorro.jpg'
@@ -33,7 +41,7 @@ def get_metadata(url):
 
 
 def add_photo(metadata):
-    return requests.post(
+    response = requests.post(
         f'{IMATCH_URL}/add',
         data={
             'url': metadata['url'],
@@ -42,6 +50,8 @@ def add_photo(metadata):
         },
         timeout=TIMEOUT
     )
+    time.sleep(ELASTIC_DELAY)
+    return response
 
 
 def search_photo(url):
@@ -64,34 +74,34 @@ def get_list():
     return requests.get(f'{IMATCH_URL}/list')
 
 
-# Fixture
-@pytest.fixture(autouse=True)
-def run_around_tests():
-    """Fixture to execute asserts before and after a test is run"""
-    yield
-    # Clean ElasticSearch Index
+def clean_elastic():
     try:
-        es.delete_by_query(index=ELASTICSEARCH_INDEX, body={"query": {"match_all": {}}}, ignore=404)
-    except Exception as e:
+        es.delete_by_query(index=ELASTICSEARCH_INDEX, body={"query": {"match_all": {}}})
+        time.sleep(1)
+    except Exception:
         pass
 
 
 # Define Tests
 def test_ping_handler():
+    clean_elastic()
     response = requests.get(f"{IMATCH_URL}/ping")
     assert response.status_code == 200
 
 
 def test_add_handler():
+    clean_elastic()
     response = add_photo(get_metadata(URL1))
     assert response.status_code == 200
 
 
 def test_delete_handler():
+    clean_elastic()
     pass
 
 
 def test_search_handler():
+    clean_elastic()
     response_add = add_photo(get_metadata(URL2))
     assert response_add.status_code == 200
     response_search = search_photo(URL2)
@@ -99,11 +109,11 @@ def test_search_handler():
     result = response_search.json()['result'][0]
     assert result['score'] == 100
     assert result['filepath'] == get_metadata(URL2)['path']
-    import ipdb; ipdb.set_trace()
     assert result['metadata'] == get_metadata(URL2)
 
 
 def test_count_handler():
+    clean_elastic()
     response_count = requests.get(f'{IMATCH_URL}/count')
     assert response_count.json()['result'] == 0
     add_photo(get_metadata(URL1))
@@ -115,4 +125,8 @@ def test_count_handler():
 
 
 def test_list_handler():
-    pass
+    clean_elastic()
+    add_photo(get_metadata(URL1))
+    add_photo(get_metadata(URL2))
+    response = get_list()
+    response.json()['result'] == [get_filepath(URL1), get_filepath(URL2)]
